1f906ffcb35f595d57fcfc4af51f7df102d13920
Nommiin <nommbot@gmail.com>
2018-01-27 01:54:29 -0600

    Completely rewrote the entirety of the script, added a "Run in GMLive" button, cleaned everything up (added some slight styling)

--- core/main.js
+++ core/main.js
@@ -1,112 +1,81 @@
 GMLux = {
-    Line: 1, Column: 0,
-    Tokens: {
-        "object": function() {
-            tHead = tPosition;
-            tTail = GMLux.SeekChar("="); tPosition = tTail;
-            tObject = tInput.substring(tHead, tTail).trim();
-            output.value += "/*GMLux Object Creation*/ " + tObject + " = ds_map_create();"; GMLux.Objects[tObject] = [];
-            tHead = GMLux.SeekChar("{"); tPosition = tHead;
-            tTail = GMLux.SeekChar("}"); tPosition = tTail;
-            GMLux.Properties(tObject, tInput.substring(tHead + 1, tTail));
-            if (tInput[tTail + 1] == ";") {tPosition++;}
-            console.log("Position: " + tPosition);
-        },
-        "function": function() {console.log("yuhuhu")}
-    },
-    Objects: [
-
-    ],
-    SeekChar: function(char, input=tInput, position=tPosition) {
-        while (input[position++] != char) {
-            if (position > tInput.length) {
-                console.error("Could not find character \"" + char + "\" in script");
-                return -1;
-            }
-        }
-        console.log("Found character \"" + char + "\" in input");
-        //tPosition = position - 1;
-        return position - 1;
-    },
-    Properties: function(object, properties, position=0) {
-        head = position;
-        while (position < properties.length) {
-            if (properties[position++] == ":") {
-                var pName = properties.substring((head), position - 1).trim();
-                head = position;
-
-                var t = 1, s = false;
-                while (properties[position++] != ",") {
-                    if (properties[position] == "\"") {
-                        console.log(properties[position - 2]);
-                        if (properties[position - 2] != "\\") {
-                            s = !s;
-                            console.log("string - " + ((s) ? "start" : "end"))
-                            
-                        }
-                    }
-
-                    if (properties[position + 1] == ",") {
-                        if (s) {position += 2;}
-                        console.log("possible end")
-                    }
-
-                    if (position >= properties.length) {
-                        t = 0;
-                        break;
-                    }
-                }
-                var pValue = properties.substring((head), position - t).trim();
-                head = position;
-
-                output.value += " " + object + "[? \"" + pName + "\"] = " + pValue + ";";
-                GMLux.Objects[object].push(pName);
-                console.log("Property: " + pName + " = " + pValue);
-            }
-            
-        }
-        //output.value += "\n";
-    },
     Transpile: function(input, output) {
-        for(tInput = input.value, tPosition = 0; tPosition < tInput.length; tPosition++) {
-            var tFound = false;
-            // Tokens
+        tInput = input.value; tOutput = output; tFound = false; tOutput.value = "";
+        for(tColumn = 0; tColumn < tInput.length; tColumn++) {
+            // Token Loop
             for(tToken in GMLux.Tokens) {
-                if (tInput.substring(tPosition, tPosition + tToken.length) == tToken) {
-                    tPosition += tToken.length;// GMLux.SeekChar(tInput, tPosition + tToken.length, "{");
+                if (tInput.substring(tColumn, tColumn + tToken.length) == tToken) {
+                    tColumn += tToken.length;
                     GMLux.Tokens[tToken]();
                     tFound = true;
                 }
-            } // End of Token Loop
+            }
+
+            // Object Loop
             for(tObject in GMLux.Objects) {
-                if (tInput.substring(tPosition, tPosition + tObject.length) == tObject) {
-                    output.value += tObject + "[? \"";
-                    tPosition = GMLux.SeekChar(".") + 1;
-                    f = false;
+                if (tInput.substring(tColumn, tColumn + tObject.length) == tObject) {
+                    tColumn = GMLux.SeekChar(".");
+                    tOutput.value += tObject + "[? ";
                     GMLux.Objects[tObject].forEach(function(tProperty) {
-                        if (tInput.substring(tPosition, tPosition + tProperty.length) == tProperty) {
-                            output.value += tProperty + "\"]";
-                            f = true;
+                        if (tInput.substring(tColumn, tColumn + tProperty.length) == tProperty) {
+                            tOutput.value += "\"" + tProperty + "\"]";
+                            tColumn += tProperty.length - 1;
+                            tFound = true;
                         }
                     });
-
-                    if (f == false) {
-                        console.error("Could not find property!");
-                    }
-                    
-                    tFound = true;
                 }
-                
             }
 
-            if (tFound == false) {
-                output.value += tInput[tPosition];
-            }
-        } // End of Input Loop
-        output.value += "\n/*GMLux Object Destruction*/ ";
-        for(tObject in GMLux.Objects) {
-            output.value += "ds_map_destroy(" + tObject + ");\n";
+            // Normal Character
+            ((tFound) ? (tFound = false) : (tOutput.value += tInput[tColumn]));
         }
-        
-    }
+    },
+    Properties: function(tObject, tProperties, tPosition=0) {
+        var tMarker = tPosition, tString = false;
+        while (tPosition < tProperties.length) {
+            while (tProperties[tPosition++] != ",") {
+                if (tPosition > tProperties.length) {
+                    break;
+                } else if (tProperties[tPosition] == "\"") {
+                    if (tProperties[tPosition - 1] != "\\") {
+                        tString = !tString;
+                    }
+                } else if (tProperties[tPosition] == ",") {
+                    if (tString) {tPosition++;}
+                }
+            }
+            GMLux.Property(tObject, tProperties.substring(tMarker, tPosition - 1).trim());
+            tMarker = tPosition;
+        }
+    },
+    Property: function(tObject, tProperty) {
+        tHead = GMLux.SeekChar(":", 0, tProperty);
+        tOutput.value += tObject + "[? \"" + tProperty.substring(0, tHead - 1) + "\"] = " + tProperty.substring(tHead, tProperty.length).trim() + "; ";
+        GMLux.Objects[tObject].push(tProperty.substring(0, tHead - 1));
+    },
+    SeekChar: function(tChar, tPosition=tColumn, _tInput=tInput) {
+        while (_tInput[tPosition++] != tChar) {
+            if (tPosition > tInput.length) {
+                console.error("Unable to find character \"" + tChar + "\" in input.");
+                break;
+            }
+        }
+        return tPosition;
+    },
+    Tokens: {
+        "object": function() {
+            // Object Name
+            tHead = tColumn;
+            tTail = GMLux.SeekChar("="); tColumn = tTail;
+            tObject = tInput.substring(tHead, tTail - 1).trim();
+            tOutput.value += "/*GMLuxCreate*/ " + tObject + " = ds_map_create(); ";
+            GMLux.Objects[tObject] = [];
+            
+            // Object Properties
+            tHead = GMLux.SeekChar("{", tTail + 1); tColumn = tHead;
+            tTail = GMLux.SeekChar("}");            tColumn = tTail;
+            GMLux.Properties(tObject, tInput.substring(tHead, tTail - 1).trim());
+        }
+    },
+    Objects: {}
 }
